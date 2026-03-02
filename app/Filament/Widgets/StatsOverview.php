e913b9f88fdd6e0221883eccf31e95e88a34ac36
abidgymnastiar <abidgymnastiar@gmail.com>
2026-03-03 00:25:15 +0700

    menambah overview di topbar pada dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,6 +5,7 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Product;
+use App\Models\Stock;
 
 
 class StatsOverview extends StatsOverviewWidget
@@ -12,10 +13,24 @@ class StatsOverview extends StatsOverviewWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Produk', Product::count())
-                ->description('Jumlah seluruh produk terdaftar')
-                ->descriptionIcon('heroicon-m-cube')
-                ->color('primary'),
+            Stat::make('Total Products', Product::count())
+                ->description('+5.2%')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+            Stat::make(
+                'Total Sisa Stok',
+                Stock::sum('closing_stock')
+            )
+                ->description('Akumulasi stok tersisa')
+                ->descriptionIcon('heroicon-m-archive-box')
+                ->color('success'),
+            Stat::make(
+                'Total Terjual',
+                Stock::sum('sold')
+            )
+                ->description('Total seluruh produk terjual')
+                ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->color('danger'),
         ];
     }
 }
