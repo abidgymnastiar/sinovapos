d82927a34852e0c0b6af964c10ddf033b2d2e804
abidgymnastiar <abidgymnastiar@gmail.com>
2026-04-30 23:36:56 +0700

    feat: implement AllSalesTable component for displaying sales data with enhanced formatting

--- app/(protected)/penjualan/seluruh/page.tsx
+++ app/(protected)/penjualan/seluruh/page.tsx
@@ -1,8 +1,15 @@
-export default function SeluruhPage() {
+import { getStocks } from "@/services/stockService";
+
+import { AllSalesTable } from "./table-core";
+
+export default async function SeluruhPage() {
+  const stocks = await getStocks();
+
   return (
-    <div>
-      <h1>Penjualan Page</h1>
-      <p>Ini adalah halaman penjualan.</p>
+    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+      <div className="px-4 lg:px-6">
+        <AllSalesTable data={stocks} />
+      </div>
     </div>
   );
 }
